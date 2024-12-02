5f67d0f05556063e534666f3f036a48a74ee6ec9
Adrien Biencourt <adrien@bncrt.com>
2024-12-02 01:19:49 +0000

    fix: use `markdownlint` relaxed style

--- lua/abiencourt/plugins/lsp-formatter-linter/nvim-lint.lua
+++ lua/abiencourt/plugins/lsp-formatter-linter/nvim-lint.lua
@@ -11,6 +11,7 @@ return {
 		},
 	},
 	config = function()
+		local nvim_lint = require("lint")
 		local filetypes = {
 			markdown = { "markdownlint", "alex", "proselint", "write_good" },
 			ghaction = { "actionlint" },
@@ -25,17 +26,22 @@ return {
 			filetypes[lang] = { "eslint_d" }
 		end
 
-		require("lint").linters_by_ft = filetypes
+		nvim_lint.linters_by_ft = filetypes
 
-		require("lint").linters.yamllint.args = {
+		nvim_lint.linters.yamllint.args = {
 			"-c",
+			"relaxed",
+		}
+
+		nvim_lint.linters.markdownlint.args = {
+			"--style",
 			"relaxed",
 		}
 
 		-- Lint on save
 		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 			callback = function()
-				require("lint").try_lint()
+				nvim_lint.try_lint()
 			end,
 		})
 	end,
