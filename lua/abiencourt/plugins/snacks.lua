43e91033113be5ce2221c6e0df9bfc6ed7a93b60
Adrien Biencourt <adrien@bncrt.com>
2024-12-08 21:13:34 +0000

    feat: uses `snacks.nvim` as dashboard

---
+++ lua/abiencourt/plugins/snacks.lua
@@ -0,0 +1,91 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		bigfile = { enabled = false },
+		notifier = { enabled = false },
+		quickfile = { enabled = false },
+		statuscolumn = { enabled = false },
+		words = { enabled = false },
+		gitbrowse = { enabled = true },
+		dashboard = {
+			enabled = true,
+			preset = {
+				keys = {
+					{
+						icon = " ",
+						key = "f",
+						desc = "Find File",
+						action = function()
+							vim.cmd("TabRename " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":~"))
+							vim.cmd("Neotree position=current")
+							vim.cmd("Telescope zoxide list")
+						end,
+					},
+					{ icon = " ", key = "N", desc = "New File", action = ":ene | startinsert" },
+					{
+						icon = " ",
+						key = "n",
+						desc = "Neovim",
+						action = function()
+							vim.cmd("tcd $XDG_CONFIG_HOME/nvim")
+							vim.cmd("TabRename Neovim Config")
+							vim.cmd("Neotree position=current")
+						end,
+					},
+					{
+						icon = " ",
+						key = "r",
+						desc = "Recent Files",
+						action = ":lua Snacks.dashboard.pick('oldfiles')",
+					},
+					{
+						icon = " ",
+						key = "d",
+						desc = "Dotfiles",
+						action = function()
+							vim.cmd("tcd $XDG_CONFIG_HOME")
+							vim.cmd("TabRename DotFiles")
+							vim.cmd("Neotree position=current")
+						end,
+					},
+					{
+						icon = "󰌵 ",
+						key = "c",
+						desc = "Chezmoi",
+						action = function()
+							vim.cmd("tcd $XDG_DATA_HOME/chezmoi")
+							vim.cmd("TabRename Chezmoi")
+							vim.cmd("Neotree position=current")
+						end,
+					},
+					{
+						icon = "󰒲 ",
+						key = "l",
+						desc = "Lazy",
+						action = ":Lazy",
+						enabled = package.loaded.lazy ~= nil,
+					},
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
+			},
+			sections = {
+				{ section = "header" },
+				{ section = "keys", gap = 1, padding = 1 },
+				{ section = "startup" },
+				{
+					section = "terminal",
+					cmd = "pokeget --hide-name --shiny rayquaza; sleep .1",
+					random = 10,
+					height = 25,
+					indent = 8,
+					pane = 2,
+				},
+			},
+		},
+	},
+}
