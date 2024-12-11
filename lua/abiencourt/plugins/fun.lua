4116109194ac799f28446702b434d2b93b2b16b0
Adrien Biencourt <adrien@bncrt.com>
2024-12-11 15:16:28 +0000

    refactor(which-key-v3): add keys for lazy loading of fun plugins

--- lua/abiencourt/plugins/fun.lua
+++ lua/abiencourt/plugins/fun.lua
@@ -1,5 +1,36 @@
 return {
-	{ "letieu/hacker.nvim", cmd = { "HackFollow" } },
-	{ "eandrju/cellular-automaton.nvim", cmd = { "CellularAutomaton" } },
+	{
+		"letieu/hacker.nvim",
+		cmd = { "HackFollow" },
+		keys = {
+			{
+				"<leader><leader>fh",
+				"<cmd>HackFollow<cr>",
+				desc = "Hack This File",
+			},
+		},
+	},
+	{
+		"eandrju/cellular-automaton.nvim",
+		cmd = { "CellularAutomaton" },
+		keys = {
+			{
+				"<leader><leader>fr",
+				"<cmd>CellularAutomaton make_it_rain<cr>",
+				desc = "Make It Rain",
+			},
+		},
+	},
+	{
+		"marcussimonsen/let-it-snow.nvim",
+		cmd = { "LetItSnow" },
+		keys = {
+			{
+				"<leader><leader>fs",
+				"<cmd>LetItSnow<cr>",
+				desc = "Let It Snow",
+			},
+		},
+		opts = {},
+	},
 }
-
