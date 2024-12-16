51b0184dff2294a8e50ad9d086ac449f16bcc4f7
Adrien Biencourt <adrien@bncrt.com>
2024-12-16 12:56:43 +0000

    feat: uses `Snacks` to toggle Treesitter-context

--- lua/abiencourt/plugins/theme/nvim-treesitter.lua
+++ lua/abiencourt/plugins/theme/nvim-treesitter.lua
@@ -1,58 +1,69 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	event = "BufReadPost",
-	keys = {
-		{
-			"<leader>T",
-			function()
-				require("treesitter-context").toggle()
-			end,
-			desc = "Toggle Treesitter Context",
-		},
-	},
-	dependencies = {
-		{ "windwp/nvim-ts-autotag", opts = { enable_close_on_slash = true } },
+	{
 		"nvim-treesitter/nvim-treesitter-context",
+		lazy = true,
+		opts = function()
+			local tsc = require("treesitter-context")
+			Snacks.toggle({
+				name = "Treesitter Context",
+				get = tsc.enabled,
+				set = function(state)
+					if state then
+						tsc.enable()
+					else
+						tsc.disable()
+					end
+				end,
+			}):map("<leader>T")
+		end,
 	},
-	config = function()
-		---@diagnostic disable-next-line: missing-fields
-		require("nvim-treesitter.configs").setup({
-			auto_install = true,
-			ensure_installed = {
-				"json",
-				"javascript",
-				"typescript",
-				"angular",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"markdown",
-				"markdown_inline",
-				"bash",
-				"lua",
-				"vim",
-				"vimdoc",
-				"dockerfile",
-				"gitignore",
-				"regex",
-				"hyprlang",
-				"git_config",
-			},
-			highlight = { enable = true },
-			autotag = { enable = false },
-			indent = { enable = true },
-			-- select blocks with <CR>
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<CR>",
-					node_incremental = "<CR>",
-					scope_incremental = "<S-CR>",
-					node_decremental = "<BS>",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = "BufReadPost",
+		dependencies = {
+			{ "windwp/nvim-ts-autotag", opts = { enable_close_on_slash = true } },
+			"nvim-treesitter/nvim-treesitter-context",
+		},
+		config = function()
+			---@diagnostic disable-next-line: missing-fields
+			require("nvim-treesitter.configs").setup({
+				auto_install = true,
+				ensure_installed = {
+					"json",
+					"javascript",
+					"typescript",
+					"angular",
+					"tsx",
+					"yaml",
+					"html",
+					"css",
+					"markdown",
+					"markdown_inline",
+					"bash",
+					"lua",
+					"vim",
+					"vimdoc",
+					"dockerfile",
+					"gitignore",
+					"regex",
+					"hyprlang",
+					"git_config",
 				},
-			},
-		})
-	end,
+				highlight = { enable = true },
+				autotag = { enable = false },
+				indent = { enable = true },
+				-- select blocks with <CR>
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<CR>",
+						node_incremental = "<CR>",
+						scope_incremental = "<S-CR>",
+						node_decremental = "<BS>",
+					},
+				},
+			})
+		end,
+	},
 }
