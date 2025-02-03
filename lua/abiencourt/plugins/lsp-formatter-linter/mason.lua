1f0fbcb7f0e214afaf4bf74482a55ab1ec26b9f4
Adrien Biencourt <adrien@bncrt.com>
2025-02-03 15:10:45 +0000

    feat: better typescript language server with `typescript-tools.nvim`

--- lua/abiencourt/plugins/lsp-formatter-linter/mason.lua
+++ lua/abiencourt/plugins/lsp-formatter-linter/mason.lua
@@ -11,6 +11,10 @@ return {
 		"b0o/schemastore.nvim",
 		"stevearc/conform.nvim",
 		"mrcjkb/rustaceanvim",
+		{
+			"pmizio/typescript-tools.nvim",
+			dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		},
 	},
 	config = function()
 		require("mason").setup({
@@ -21,11 +25,6 @@ return {
 
 		-- Auto update schemastore at startup
 		require("lazy").update({ plugins = { "schemastore.nvim" }, show = false })
-
-		-- see https://github.com/rcarriga/nvim-dap-ui/issues/367
-		-- require("neodev").setup({
-		-- 	library = { plugins = { "nvim-dap-ui" }, types = true },
-		-- })
 
 		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
@@ -84,6 +83,58 @@ return {
 			},
 		}
 
+		require("typescript-tools").setup({
+			-- on_attach = function() ... end,
+			handlers = {
+				capabilities = lsp_capabilities, -- Needs to be added manually for each LSP
+			},
+			settings = {
+				-- spawn additional tsserver instance to calculate diagnostics on it
+				separate_diagnostic_server = true,
+				-- "change"|"insert_leave" determine when the client asks the server about diagnostic
+				publish_diagnostic_on = "insert_leave",
+				-- array of strings("fix_all"|"add_missing_imports"|"remove_unused"|
+				-- "remove_unused_imports"|"organize_imports") -- or string "all"
+				-- to include all supported code actions
+				-- specify commands exposed as code_actions
+				expose_as_code_action = {},
+				-- string|nil - specify a custom path to `tsserver.js` file, if this is nil or file under path
+				-- not exists then standard path resolution strategy is applied
+				tsserver_path = nil,
+				-- specify a list of plugins to load by tsserver, e.g., for support `styled-components`
+				-- (see 💅 `styled-components` support section)
+				tsserver_plugins = {},
+				-- this value is passed to: https://nodejs.org/api/cli.html#--max-old-space-sizesize-in-megabytes
+				-- memory limit in megabytes or "auto"(basically no limit)
+				tsserver_max_memory = "auto",
+				-- described below
+				tsserver_format_options = {},
+				tsserver_file_preferences = {
+					includeInlayParameterNameHints = "all",
+				},
+				-- locale of all tsserver messages, supported locales you can find here:
+				-- https://github.com/microsoft/TypeScript/blob/3c221fc086be52b19801f6e8d82596d04607ede6/src/compiler/utilitiesPublic.ts#L620
+				tsserver_locale = "en",
+				-- mirror of VSCode's `typescript.suggest.completeFunctionCalls`
+				complete_function_calls = false,
+				include_completions_with_insert_text = true,
+				-- CodeLens
+				-- WARNING: Experimental feature also in VSCode, because it might hit performance of server.
+				-- possible values: ("off"|"all"|"implementations_only"|"references_only")
+				code_lens = "off",
+				-- by default code lenses are displayed on all referencable values and for some of you it can
+				-- be too much this option reduce count of them by removing member references from lenses
+				disable_member_code_lens = true,
+				-- JSXCloseTag
+				-- WARNING: it is disabled by default (maybe you configuration or distro already uses nvim-ts-autotag,
+				-- that maybe have a conflict if enable this feature. )
+				jsx_close_tag = {
+					enable = false,
+					filetypes = { "javascriptreact", "typescriptreact" },
+				},
+			},
+		})
+
 		local lspconfig = require("lspconfig")
 		require("mason-lspconfig").setup_handlers({
 			function(server_name)
@@ -96,6 +147,10 @@ return {
 			-- Rust
 			-- Do not spin up rust-analyzer automatically because it starts with rustacenvim
 			["rust_analyzer"] = function() end,
+
+			-- TSServer
+			-- Do not spin up ts_ls automatically because it starts with typescript-tools
+			["ts_ls"] = function() end,
 
 			-- Cypress Cucumber preprocessor https://github.com/badeball/cypress-cucumber-preprocessor
 			["cucumber_language_server"] = function()
@@ -118,39 +173,6 @@ return {
 						Lua = {
 							hint = {
 								enable = true,
-							},
-						},
-					},
-				})
-			end,
-
-			-- TSServer
-			["ts_ls"] = function()
-				lspconfig.ts_ls.setup({
-					capabilities = lsp_capabilities, -- Needs to be added manually for each LSP
-					settings = {
-						typescript = {
-							inlayHints = {
-								includeInlayParameterNameHints = "all",
-								includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-								includeInlayFunctionParameterTypeHints = true,
-								includeInlayVariableTypeHints = true,
-								includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-								includeInlayPropertyDeclarationTypeHints = true,
-								includeInlayFunctionLikeReturnTypeHints = true,
-								includeInlayEnumMemberValueHints = true,
-							},
-						},
-						javascript = {
-							inlayHints = {
-								includeInlayParameterNameHints = "all",
-								includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-								includeInlayFunctionParameterTypeHints = true,
-								includeInlayVariableTypeHints = true,
-								includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-								includeInlayPropertyDeclarationTypeHints = true,
-								includeInlayFunctionLikeReturnTypeHints = true,
-								includeInlayEnumMemberValueHints = true,
 							},
 						},
 					},
