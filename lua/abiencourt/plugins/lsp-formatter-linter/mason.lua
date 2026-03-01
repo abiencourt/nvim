85e9a086647cbe41da1c9698f355413308e4d3fd
Adrien Biencourt <adrien@bncrt.com>
2026-03-01 12:45:56 +0000

    refactor(lsp): simplify LSP server setup and use vim.lsp.config
    - Replace lspconfig and handler-based setup with direct use of vim.lsp.config for each server.
    - Use more concise inlayHints and settings tables.
    - Update Mason LSP config initialization.

--- lua/abiencourt/plugins/lsp-formatter-linter/mason.lua
+++ lua/abiencourt/plugins/lsp-formatter-linter/mason.lua
@@ -23,50 +23,26 @@ return {
 		require("lazy").update({ plugins = { "schemastore.nvim" }, show = false })
 
 		local lsp_capabilities = require("blink.cmp").get_lsp_capabilities()
-
-		-- Add folding capabilities required by ufo.nvim
 		lsp_capabilities.textDocument.foldingRange = {
 			dynamicRegistration = false,
 			lineFoldingOnly = true,
 		}
 
-		-- local lsp_attach = function(client, bufnr)
-		--              -- Create your keybindings here...
-		--              -- Could it be used to display the name of the schema?
-		--              -- SchemaStore.nvim cannot do it for us
-		-- end
-
-		-- Adds capabilities to rustaceanvim
+		-- Rustaceanvim custom capabilities
 		vim.g.rustacenvim = {
 			server = {
 				capabilities = lsp_capabilities,
 				settings = {
 					["rust-analyzer"] = {
 						inlayHints = {
-							bindingModeHints = {
-								enable = false,
-							},
-							chainingHints = {
-								enable = true,
-							},
-							closingBraceHints = {
-								enable = true,
-								minLines = 25,
-							},
-							closureReturnTypeHints = {
-								enable = "never",
-							},
-							lifetimeElisionHints = {
-								enable = "never",
-								useParameterNames = false,
-							},
+							bindingModeHints = { enable = false },
+							chainingHints = { enable = true },
+							closingBraceHints = { enable = true, minLines = 25 },
+							closureReturnTypeHints = { enable = "never" },
+							lifetimeElisionHints = { enable = "never", useParameterNames = false },
 							maxLength = 25,
-							parameterHints = {
-								enable = true,
-							},
-							reborrowHints = {
-								enable = "never",
-							},
+							parameterHints = { enable = true },
+							reborrowHints = { enable = "never" },
 							renderColons = true,
 							typeHints = {
 								enable = true,
@@ -79,117 +55,94 @@ return {
 			},
 		}
 
-		local lspconfig = require("lspconfig")
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				lspconfig[server_name].setup({
-					-- on_attach = lsp_attach,
-					capabilities = lsp_capabilities,
-				})
-			end,
+		-- Native LSP configuration for each server
+		local vim_lsp_config = vim.lsp.config
 
-			-- Rust
-			-- Do not spin up rust-analyzer automatically because it starts with rustacenvim
-			["rust_analyzer"] = function() end,
+		-- Cypress Cucumber preprocessor
+		vim_lsp_config("cucumber_language_server", {
+			capabilities = lsp_capabilities,
+			settings = {
+				cucumber = {
+					features = { "cypress/e2e/**/*.feature" },
+					glue = { "cypress/e2e/**/*.ts" },
+				},
+			},
+		})
 
-			-- Cypress Cucumber preprocessor https://github.com/badeball/cypress-cucumber-preprocessor
-			["cucumber_language_server"] = function()
-				lspconfig.cucumber_language_server.setup({
-					capabilities = lsp_capabilities, -- Needs to be added manually for each LSP
-					settings = {
-						cucumber = {
-							features = { "cypress/e2e/**/*.feature" },
-							glue = { "cypress/e2e/**/*.ts" },
-						},
+		-- Lua
+		vim_lsp_config("lua_ls", {
+			capabilities = lsp_capabilities,
+			settings = {
+				Lua = { hint = { enable = true } },
+			},
+		})
+
+		-- TSServer
+		vim_lsp_config("ts_ls", {
+			capabilities = lsp_capabilities,
+			settings = {
+				typescript = {
+					inlayHints = {
+						includeInlayParameterNameHints = "all",
+						includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+						includeInlayFunctionParameterTypeHints = true,
+						includeInlayVariableTypeHints = true,
+						includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+						includeInlayPropertyDeclarationTypeHints = true,
+						includeInlayFunctionLikeReturnTypeHints = true,
+						includeInlayEnumMemberValueHints = true,
 					},
-				})
-			end,
-
-			-- Lua
-			["lua_ls"] = function()
-				lspconfig.lua_ls.setup({
-					capabilities = lsp_capabilities, -- Needs to be added manually for each LSP
-					settings = {
-						Lua = {
-							hint = {
-								enable = true,
-							},
-						},
+				},
+				javascript = {
+					inlayHints = {
+						includeInlayParameterNameHints = "all",
+						includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+						includeInlayFunctionParameterTypeHints = true,
+						includeInlayVariableTypeHints = true,
+						includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+						includeInlayPropertyDeclarationTypeHints = true,
+						includeInlayFunctionLikeReturnTypeHints = true,
+						includeInlayEnumMemberValueHints = true,
 					},
-				})
-			end,
+				},
+			},
+		})
 
-			-- TSServer
-			["ts_ls"] = function()
-				lspconfig.ts_ls.setup({
-					capabilities = lsp_capabilities, -- Needs to be added manually for each LSP
-					settings = {
-						typescript = {
-							inlayHints = {
-								includeInlayParameterNameHints = "all",
-								includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-								includeInlayFunctionParameterTypeHints = true,
-								includeInlayVariableTypeHints = true,
-								includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-								includeInlayPropertyDeclarationTypeHints = true,
-								includeInlayFunctionLikeReturnTypeHints = true,
-								includeInlayEnumMemberValueHints = true,
-							},
-						},
-						javascript = {
-							inlayHints = {
-								includeInlayParameterNameHints = "all",
-								includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-								includeInlayFunctionParameterTypeHints = true,
-								includeInlayVariableTypeHints = true,
-								includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-								includeInlayPropertyDeclarationTypeHints = true,
-								includeInlayFunctionLikeReturnTypeHints = true,
-								includeInlayEnumMemberValueHints = true,
-							},
-						},
+		-- JSON
+		vim_lsp_config("jsonls", {
+			capabilities = lsp_capabilities,
+			settings = {
+				json = {
+					schemas = require("schemastore").json.schemas(),
+					validate = { enable = true },
+				},
+			},
+		})
+
+		-- YAML
+		vim_lsp_config("yamlls", {
+			capabilities = lsp_capabilities,
+			settings = {
+				yaml = {
+					keyOrdering = false,
+					schemaStore = {
+						enable = false,
+						url = "",
 					},
-				})
-			end,
+					schemas = require("schemastore").yaml.schemas(),
+				},
+			},
+		})
 
-			-- JSON
-			["jsonls"] = function()
-				lspconfig.jsonls.setup({
-					capabilities = lsp_capabilities, -- Needs to be added manually for each LSP
-					settings = {
-						json = {
-							schemas = require("schemastore").json.schemas(),
-							validate = { enable = true },
-						},
-					},
-				})
-			end,
+		-- Python
+		vim_lsp_config("pyright", {
+			capabilities = lsp_capabilities,
+		})
 
-			-- YAML
-			["yamlls"] = function()
-				lspconfig.yamlls.setup({
-					capabilities = lsp_capabilities, -- Needs to be added manually for each LSP
-					settings = {
-						yaml = {
-							keyOrdering = false,
-							schemaStore = {
-								-- You must disable built-in schemaStore support if you want to use this plugin and its advanced options like `ignore`.
-								enable = false,
-								-- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-								url = "",
-							},
-							schemas = require("schemastore").yaml.schemas(),
-						},
-					},
-				})
-			end,
-
-			-- Python
-			["pyright"] = function()
-				lspconfig.pyright.setup({
-					capabilities = lsp_capabilities, -- Needs to be added manually for each LSP
-				})
-			end,
+		require("mason-lspconfig").setup({
+			-- By default, automatic_enable is true.
+			-- Set to false if you want to manually enable servers.
+			-- automatic_enable = true,
 		})
 	end,
 }
